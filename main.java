e2b7a492c246132dd74fcc0a612888c4132cb813
Sinchana  V <sinchanavgowdasinchu@gmail.com>
2025-10-19 13:32:08 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        byte numberOfCountries = 110;
+        System.out.println("Netflix is available in " + numberOfCountries + " countries.");
+        
+        short availableTitles = 15000;
+        System.out.println("Netflix offers " + availableTitles + " unique titles worldwide.");
+        
+        int totalSubscribers = 200000000;
+        System.out.println("Netflix has " + totalSubscribers + " subscribers globally.");
+        
+        long streamingMinutes = 100000000000L;
+        System.out.println("Total streaming minutes on Netflix last year: " + streamingMinutes);
+    }
+}
